19f663183fff1d2b7d24631ff7e2e49ee6642f56
simon <romansmithd@gmail.com>
2025-11-01 17:42:20 +0800

    modify index.js

--- server/routes/index.js
+++ server/routes/index.js
@@ -5,7 +5,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
 #  res.render('index', { title: 'World' });
-  res.send('My Student ID is 21369766. Welcome to Express');
+  res.send('My Student ID is 21369766.   Welcome to Express');
 
 });
 
