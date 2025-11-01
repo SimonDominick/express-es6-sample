dff6457d06e6f390567ecc7c9ff6bc4e761a26e4
SimonDominick <42027551+SimonDominick@users.noreply.github.com>
2025-11-01 22:48:56 +0800

    Update index.js

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,7 +4,6 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-#  res.render('index', { title: 'World' });
   res.send('My Student ID is 21369766.   Welcome to Express');
 
 });
